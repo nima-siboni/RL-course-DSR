475950fb1d5e337e66ca73fd0d649382cb2671ec
Nima H. Siboni <nima@paretos.ai>
2021-06-14 23:50:05 +0200

    readability improved:
    [Modified] simple_world_Monte_Carlo/control-solution.py: comments are made clear.

--- simple_world_Monte_Carlo/control-solution.py
+++ simple_world_Monte_Carlo/control-solution.py
@@ -9,6 +9,29 @@ from rl_utils import return_a_random_policy, choose_an_action_based_on_pi
 from plot_utils import create_plot, plotter, plot_simulation, plot_the_policy
 from utils import dstack_product
 
+# The code structure:
+# 1. Initialization
+## 1.1 inputs and the random seed
+## 1.2  setting the geometry
+## 1.3 create the environment
+## 1.4 create a random policy
+## 1.5 some important paramters
+## 1.6 setting up the plot
+## 1.7 create an array for all states
+#_____________________________________
+
+# 2. Evaluate the current policy
+## 2.1 a sweep over all the states in the system.
+### 2.1.1 for each state, restart the episode
+### 2.1.2 run the simulation (following pi) and collect all the rewards
+#______________________________________
+
+# 3. Policy Improvement
+## 3.1 let's first find out the best action for each point
+### 3.1.1 calculate the Qs
+## 3.2 convert the found best actions to a soft but greedy policy
+#______________________________________
+
 # 1. Initialization
 
 # 1.1 inputs and the random seed
