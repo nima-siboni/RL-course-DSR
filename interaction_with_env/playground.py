f338af82613498c1e043ed8839647030fddc00fc
nima-siboni <nima.siboni@gmail.com>
2022-11-10 20:07:01 +0100

    requirements updated and playgroud for env is added:
    [Modified] requirements.txt: pygame is added
    
    [Added] playground.py: a playground for interaction with env is added.

---
+++ interaction_with_env/playground.py
@@ -0,0 +1,16 @@
+import gym
+# Create an environment
+env = gym.make("CartPole-v1")
+
+# 0 - Reset the environment
+
+# 1 - Let's examine the env
+# 1.1 - Check the observation space of the agent
+# 1.2 - Check the action space of the agent
+# 1.3 - Render the env
+# 1.4 - Reset the environment
+# 1.5 - Check the source code of the env.
+
+# 2 - Interact with environment
+# 2.1 - Push the cart to the left once
+# 2.2 - Repeat the action above until the pole falls, i.e. the episode is terminated
