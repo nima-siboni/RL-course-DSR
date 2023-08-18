369f22e2d8a9e3030cd2abe84dacbdc33825d97e
nima-siboni <nima.siboni@gmail.com>
2023-08-18 19:59:21 +0200

    fix: first learning agent

--- your_1st_agents/learning_agent.py
+++ your_1st_agents/learning_agent.py
@@ -1,27 +1,22 @@
-# 0 - Choose an alogrithm from ray.rllib.algorithms,
-# e.g. import ray.rllib.algorithms.xxx as xxx
+# 0 - Choose an alogrithm from ray.rllib.algorithms, e.g. ray.rllib.algorithms.xxx as
+# xxx
+# 1 - Configure the xxx algorithm
+# 1.1 - Convert the config to a dict by config.to_dict()
+# 1.2 - Examine the config and modify it if needed, e.g. change the "learnign rate"
+# to 0.0001, or the framework, or env
+# 2 - Create an agent with .build, train it, and examine the training reports report = agent.train()
+# 3 - Train the agent,
 
-# 1 - Import ray and initialize it with ray.init()
+# 4 - Run a loop for nr_trainings = 50 times agent.train()
 
-# 2 - Configure the xxx algorithm
-# 2.1 - Get the default config of xxx from xxx.DEFAULT_CONFIG.copy()
-# 2.2 - Examine the config and modify it if needed, e.g. change the "num_gpus" to 0,
-# and the learning_rate to 0.001
-
-# 3 - Create an agent
-# 3.1 - Creating the agent wth config and env; xxx.XXX(config=config, env="CartPole-v1")
-
-# 4 - Train the agent, and examine the training reports: report = agent.train()
-
-# 5 - Run a loop for nr_trainings = 50 times agent.train()
-
-# 6 - Visualize the trained agent; This is similar to running the random_agent,
+# 5 - Visualize the trained agent; This is similar to running the random_agent,
 # except that this time we have a trained agent
-# 6.1 - Create the an environment similar to the training env.
-# 6.2. Let the agent choose an action;
-# 6.3. and pass it to the environment
-# 6.4. How much reward did you get for that action? Keep the score!
-# 6.5. Repeat the 6.{2,3, 4} until the end of the episode
-# 6.6. How much total reward you got? What does it mean to have large/small reward?
+# 5.1 - Create an environment similar to the training env.
+    # 5.2. Let the agent choose an action;
+    # 5.3. and pass it to the environment
+    # 5.4. How much reward did you get for that action? Keep the score!
+    # 5.5. Repeat the 5.{2,3, 4} until the end of the episode
+    # visualize the agent
+    # continue with the next step without closing the plot
 
-print("Good-bye.")
+# 5.6. How much total reward you got? What does it mean to have large/small reward?
--- your_1st_agents/learning_agent_solution.py
+++ your_1st_agents/learning_agent_solution.py
@@ -1,33 +1,28 @@
-# 0 - Choose an alogrithm from ray.rllib.algorithms, e.g. ray.rllib.algorithms.xxx as
-# xxx
-# 1 - Import ray and initialize it with ray.init()
-# 2 - Configure the xxx algorithm
-# 2.1 - Get the default config of xxx from xxx.DEFAULT_CONFIG.copy()
-# 2.2 - Examine the config and modify it if needed, e.g. change the "num_gpus" to 0,
-# and the learning_rate to 0.001
 import gymnasium as gym
 from your_1st_agents.plot_util import visualize_env
-
-# 0 - Choose an algorithm from ray.rllib.algorithms
+# 0 - Choose an alogrithm from ray.rllib.algorithms, e.g. ray.rllib.algorithms.xxx as
+# xxx
 from ray.rllib.algorithms.dqn import DQNConfig
 
-
-# 1- Configure the blah algorithm
-# 1.1. get the default config for you algorithm
+# 1 - Configure the xxx algorithm
+# 1.1 - Convert the config to a dict by config.to_dict()
+# 1.2 - Examine the config and modify it if needed, e.g. change the "num_gpus" to 0,
+# and the learning_rate to 0.001, the environment with .environment(env="CartPole-v1"), the
+# the framework
 config = DQNConfig()
 # 1.2 - Examine the config and modify it if needed, e.g. change the "learnign rate"
 # to 0.0001,
 config_as_dict = config.to_dict()
-
-# 2 - Create an agent, and examine the training reports: report = agent.train()
 config.environment(env="CartPole-v1")
+config.framework("tf")
+# 2 - Create an agent with .build, train it, and examine the training reports report = agent.train()
 agent = config.build()
 # 3 - Train the agent,
 reports = agent.train()
 print(reports)
 
 # 4 - Run a loop for nr_trainings = 50 times agent.train()
-nr_trainings = 1
+nr_trainings = 5
 for _ in range(nr_trainings):
     reports = agent.train()
     print(_, reports["episode_reward_mean"])
