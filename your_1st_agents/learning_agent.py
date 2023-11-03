6a9875fa03d1272adc451808b742e0d680b5e44d
nima-siboni <nima.siboni@gmail.com>
2023-11-03 19:44:12 +0100

    updating the learning_agent

--- your_1st_agents/learning_agent.py
+++ your_1st_agents/learning_agent.py
@@ -1,27 +1,21 @@
-# 0 - Choose an alogrithm from ray.rllib.algorithms,
-# e.g. import ray.rllib.algorithms.xxx as xxx
+"""Your first learning agent: template."""
+# 0 - Choose an algorithm from ray.rllib.algorithms, e.g. ray.rllib.algorithms.xxx as
+# xxxConfig
+# 1 -  Build an agent
+# 1.1 - Get the default config of xxxConfig()
+# 1.2 - Examine the config by converting it to a dict via .to_dict() method
+# 1.3 - Modify the config if needed, e.g. change the "num_gpus" to 0,
+# 1.4 - Introduce the environment to the agent's config
+# 1.5 - Build the agent
+# 2 - Train the agent for one training round and get the reports
+# 3 - Run a loop for nr_trainings = 50 times
 
-# 1 - Import ray and initialize it with ray.init()
-
-# 2 - Configure the xxx algorithm
-# 2.1 - Get the default config of xxx from xxx.DEFAULT_CONFIG.copy()
-# 2.2 - Examine the config and modify it if needed, e.g. change the "num_gpus" to 0,
-# and the learning_rate to 0.001
-
-# 3 - Create an agent
-# 3.1 - Creating the agent wth config and env; xxx.XXX(config=config, env="CartPole-v1")
-
-# 4 - Train the agent, and examine the training reports: report = agent.train()
-
-# 5 - Run a loop for nr_trainings = 50 times agent.train()
-
-# 6 - Visualize the trained agent; This is similar to running the random_agent,
+# 4 - Visualize the trained agent; This is similar to running the random_agent,
 # except that this time we have a trained agent
-# 6.1 - Create the an environment similar to the training env.
-# 6.2. Let the agent choose an action;
-# 6.3. and pass it to the environment
-# 6.4. How much reward did you get for that action? Keep the score!
-# 6.5. Repeat the 6.{2,3, 4} until the end of the episode
-# 6.6. How much total reward you got? What does it mean to have large/small reward?
-
+# 4.1 - Create an environment similar to the training env.
+# 4.2. Let the agent choose an action;
+# 4.3. and pass it to the environment
+# 4.4. How much reward did you get for that action? Keep the score!
+# 4.5. Repeat the 4.{2,3, 4} until the end of the episode
+# 4.6. How much total reward you got? What does it mean to have large/small reward?
 print("Good-bye.")
--- your_1st_agents/learning_agent_solution.py
+++ your_1st_agents/learning_agent_solution.py
@@ -1,60 +1,77 @@
-# 0 - Choose an alogrithm from ray.rllib.algorithms, e.g. ray.rllib.algorithms.xxx as
-# xxx
-# 1 - Import ray and initialize it with ray.init()
-# 2 - Configure the xxx algorithm
-# 2.1 - Get the default config of xxx from xxx.DEFAULT_CONFIG.copy()
-# 2.2 - Examine the config and modify it if needed, e.g. change the "num_gpus" to 0,
-# and the learning_rate to 0.001
+"""Your first learning agent."""
 import gymnasium as gym
-from your_1st_agents.plot_util import visualize_env
-
-# 0 - Choose an algorithm from ray.rllib.algorithms
 from ray.rllib.algorithms.dqn import DQNConfig
 
+from your_1st_agents.plot_util import visualize_env
 
-# 1- Configure the blah algorithm
-# 1.1. get the default config for you algorithm
+# 0 - Choose an algorithm from ray.rllib.algorithms, e.g. ray.rllib.algorithms.xxx as
+# xxxConfig
+# 1 -  Build an agent
+# 1.1 - Get the default config of xxxConfig()
+# 1.2 - Examine the config by converting it to a dict via .to_dict() method
+# 1.3 - Modify the config if needed, e.g. change the "num_gpus" to 0,
+# 1.4 - Introduce the environment to the agent's config
+# 1.5 - Build the agent
+# 2 - Train the agent for one training round and get the reports
+# 3 - Run a loop for nr_trainings = 50 times
+
+# 4 - Visualize the trained agent; This is similar to running the random_agent,
+# except that this time we have a trained agent
+# 4.1 - Create an environment similar to the training env.
+# 4.2. Let the agent choose an action;
+# 4.3. and pass it to the environment
+# 4.4. How much reward did you get for that action? Keep the score!
+# 4.5. Repeat the 4.{2,3, 4} until the end of the episode
+# 4.6. How much total reward you got? What does it mean to have large/small reward?
+
+
+# 1 - Build an agent
+# 1.1 - Get the default config of xxxConfig()
+
 config = DQNConfig()
-# 1.2 - Examine the config and modify it if needed, e.g. change the "learnign rate"
-# to 0.0001,
+# 1.2 - Examine the config by converting it to a dict via .to_dict() method
 config_as_dict = config.to_dict()
 
-# 2 - Create an agent, and examine the training reports: report = agent.train()
-config.environment(env="CartPole-v1")
+# 1.3 - Modify the config if needed, e.g. change the "num_gpus" to 0, or change the learning_rate
+# (lr)
+# 1.4 - Introduce the environment to the agent's config
+config.environment(env="CartPole-v1").framework(framework="tf")
+# 1.5 - Build the agent from the config with .build()
+
 agent = config.build()
-# 3 - Train the agent,
+# 2 - Train the agent for one training round with .train and get the reports
 reports = agent.train()
 print(reports)
 
-# 4 - Run a loop for nr_trainings = 50 times agent.train()
-nr_trainings = 1
+# 3 - Run a loop for nr_trainings = 50 times
+nr_trainings = 1  # pylint: disable=invalid-name
 for _ in range(nr_trainings):
     reports = agent.train()
     print(_, reports["episode_reward_mean"])
 
-# 5 - Visualize the trained agent; This is similar to running the random_agent,
+# 4 - Visualize the trained agent; This is similar to running the random_agent,
 # except that this time we have a trained agent
-# 5.1 - Create an environment similar to the training env.
+# 4.1 - Create an environment similar to the training env.
 env = gym.make("CartPole-v1", render_mode="rgb_array")
 s, _ = env.reset()
-done = False
-cumulative_reward = 0
+done = False  # pylint: disable=invalid-name
+cumulative_reward = 0  # pylint: disable=invalid-name
 
 while not done:
-    # 5.2. Let the agent choose an action;
+    # 4.2. Let the agent choose an action;
     a = agent.compute_single_action(observation=s, explore=False)
-    # 5.3. and pass it to the environment
+    # 4.3. and pass it to the environment
     s, r, terminated, truncated, info = env.step(action=a)
 
-    # 5.4. How much reward did you get for that action? Keep the score!
+    # 4.4. How much reward did you get for that action? Keep the score!
     cumulative_reward += r
     done = terminated or truncated
-    # 5.5. Repeat the 5.{2,3, 4} until the end of the episode
+    # 4.5. Repeat the 4.{2,3, 4} until the end of the episode
     # visualize the agent
     visualize_env(env=env, pause_sec=0.1)
     # continue with the next step without closing the plot
 
-# 5.6. How much total reward you got? What does it mean to have large/small reward?
+# 4.6. How much total reward you got? What does it mean to have large/small reward?
 print("Total reward:", cumulative_reward)
 
 print("Good-bye.")
